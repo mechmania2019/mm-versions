5e8c7839dd42364832e24688e085cef5790fbeb3
Pranay Prakash <pranay.gp@gmail.com>
2018-09-20 23:33:20 -0500

    auth

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const { Script } = require("mm-schemas")(mongoose);
 mongoose.connect(process.env.MONGO_URL);
 mongoose.Promise = global.Promise;
 
-module.exports = async (req, res) => {
+module.exports = authenticate(async (req, res) => {
   const team = req.user;
   console.log(`${team.name} - Getting script versions`);
   const scripts = await Script.find({ owner: team._id }).exec();
   return scripts;
-};
+});
