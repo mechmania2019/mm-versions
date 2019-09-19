c690f309b352bb2014ddd35733e983375d38e6fe
angiecheng <angie.cheng33@gmail.com>
2019-09-18 19:06:08 -0500

    corrected send and Mongo deprecations

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const { Script } = require("mm-schemas")(mongoose);
 
 const send = (res, status, data) => (res.statusCode = status, res.end(data));
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 
 module.exports = authenticate(async (req, res) => {
   const team = req.user;
   console.log(`${team.name} - Getting script versions`);
   const scripts = await Script.find({ owner: team._id }).sort("-_id").exec();
-  return scripts;
+  send(res, 200, JSON.stringify(scripts));
 });
